17e1292853e69694b5e8b72b93034a1a034350ad
Krody Robert <robi@presslabs.com>
2013-07-31 12:19:18 +0300

    Simplyfied call for render history function

--- git2wp_render.php
+++ git2wp_render.php
@@ -1,25 +1,26 @@
 <?php
 
-	function git2wp_render_resource_history( $reource ) {
-		$html = '';
-		$html .= "<div height='100px'>";
+
+	function git2wp_render_resource_history( $resource ) { ?>
+		<div height='100px'>
 		
+		<?php
 		$commit_history = $resource['git_data']['commit_history'];
 		$commit_history = array_reverse($commit_history);
 		
-		foreach($commit_history as $key => $commit) {
-			
-			$html .= "<label for='downgrade_resource_".$key."'>";
-			$html .= "<div>";
-			
-			$html .= "<a href='".$commit['git_url']."' target='_blank'>".substr($commit['sha'],0,7)."</a>";
-			$html .= "<span style='padding-left: 5px;'>".ucfirst($commit['message'])."</span>";
-			$html .= "<span style='padding-left: 5px;'>".date("d-m-y", $commit['timestamp'])."</span>";
-			
-			$html .= "</div>";
-			$html .= "</label>";
+		foreach($commit_history as $key => $commit) { ?>
+			<label for="downgrade_resource_<?php echo $key; ?>">
+				<div>
+					<a href="<?php echo $commit['git_url']; ?>" target='_blank'><?php echo substr($commit['sha'], 0, 7); ?></a>
+					<span style='padding-left: 5px;'><?php echo ucfirst($commit['message']); ?></span>
+					<span style='padding-left: 5px;'><?php echo date("d-m-y", $commit['timestamp']); ?></span>
+				</div>
+			</label>
+		<?php
 		}
-		
-		$html .= "</div>";
+		?>
+		</div>
+	<?php	
 	}
+	
 ?>
