28a3b7a98c4189b54925f3a5959aa6d4b68ea0f0
Krody Robert <robi@presslabs.com>
2013-08-12 13:05:01 +0300

    Reworked history timestamp render

--- git2wp_render.php
+++ git2wp_render.php
@@ -19,20 +19,15 @@
 					
 					foreach($commit_history as $commit) {
 						$k++;
-						$str_date = '';
-						$date = date_parse($commit['timestamp']);
-						$date['day'] = str_pad($date['day'], 2, '0' , STR_PAD_LEFT);
-						$date['month'] = str_pad($date['month'], 2, '0', STR_PAD_LEFT);
-						$date['hour'] = str_pad($date['hour'], 2, '0', STR_PAD_LEFT);
-						$date['minute'] = str_pad($date['minute'], 2, '0', STR_PAD_LEFT);
 						
-						$str_date .= $date['day'].".".$date['month'].".".$date['year']."   ".$date['hour'].":".$date['minute'];
+						$date_time = new DateTime($commit['timestamp']);
+						$date_time =  $date_time->format("d.m.y H:i:s");
 						?>
 						
 								<tr class="<?php echo ($k % 2) ? 's-inactive' : '';?>">
 									<td width="10%;"><a href="<?php echo $commit['git_url']; ?>" target='_blank'><?php echo substr($commit['sha'], 0, 7); ?></a></td>
 									<td width="70%;"><?php echo ucfirst($commit['message']); ?></td>
-									<td width="10%;"><?php echo $str_date; ?></td>
+									<td width="10%;"><?php echo $date_time; ?></td>
 									<td width="10%;"><input type='submit' value='Revert' class="downgrade button-secondary" id="downgrade-resource-<?php echo $resource_id."-".$commit['sha']; ?>" /></td>
 								</tr>
 
