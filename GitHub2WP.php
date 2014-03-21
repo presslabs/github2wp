131fb25ab5836845bab8072b0ada486f98bff7b3
krodyrobi <robi@presslabs.com>
2014-03-21 17:21:07 +0200

    Removed unnecessary terms from constants. Fixed action hooks callables.

--- GitHub2WP.php
+++ GitHub2WP.php
@@ -1,40 +1,45 @@
 <?php
 
 class GitHub2WP {
-    const PLUGIN_NAME = 'github2wp';
-    const PLUGIN_WP_IDENTIFIER = 'github2wp/ini.php';
-    const PLUGIN_PREFIX = 'gh2w_';
+    const NAME = 'github2wp';
+    const WP_IDENTIFIER = 'github2wp/ini.php';
+    const WP_BASE_IDENTIFIER = 'ini.php';
+    const PREFIX = 'gh2w_';
+
 
     public function __construct() {
         $this->init();
     }
 
+
     private function init() {
-        register_activation_hook( __FILE__, 'activate' );
-        register_deactivation_hook( __FILE__, 'deactivate' );
-        add_option( GitHub2WP::PLUGIN_PREFIX . '_options', array() );
-        add_action( 'uninstall_' . plugin_basename( __FILE__ ), array( $this, 'uninstall' ) );
+        register_activation_hook( GitHub2WP::WP_BASE_IDENTIFIER, array( $this, 'activate' ) );
+        register_deactivation_hook( GitHub2WP::WP_BASE_IDENTIFIER, array( $this, 'deactivate') );
+        add_action( 'uninstall_' . GitHub2WP::WP_BASE_IDENTIFIER, array( $this, 'uninstall' ) );
+        add_option( GitHub2WP::PREFIX . '_options', array() );
     }
 
 
     private function activate() {
-        wp_schedule_event( current_time ( 'timestamp' ), '6h', GitHub2WP::PLUGIN_PREFIX . 'cron_hook' );
+        wp_schedule_event( current_time ( 'timestamp' ), '6h', GitHub2WP::PREFIX . 'cron_hook' );
     }
 
 
 
     private function deactivate() {
-        wp_clear_scheduled_hook( GitHub2WP::PLUGIN_PREFIX . 'cron_hook' );
+        wp_clear_scheduled_hook( GitHub2WP::PREFIX . 'cron_hook' );
     }
 
 
 
     private function uninstall() {
         $this->delete_options();
-        //delete_transient( GitHub2WP::PLUGIN_PREFIX . 'branches' );
+        delete_transient( GitHub2WP::PREFIX . 'branches' );
     }
 
+
     private function delete_options() {
+        delete_option( GitHub2WP::PREFIX . '_options' );
     }
 
 } 
