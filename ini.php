975841fc92da515b1f223e70f5ed6531b0c2f61a
krodyrobi <robi@presslabs.com>
2014-03-26 15:56:06 +0200

    Added auto loading based on namespaces.

--- ini.php
+++ ini.php
@@ -1,13 +1,25 @@
 <?php
-/*
- * Plugin Name: github2wp
- * Plugin URI: http://wordpress.org/extend/plugins/github2wp/
- * Description: Managing themes and plugins from github.
- * Author: PressLabs
- * Author URI: http://www.presslabs.com/
- * Version: 1.0.0
- */
+    /*
+     * Plugin Name: github2wp
+     * Plugin URI: http://wordpress.org/extend/plugins/github2wp/
+     * Description: Managing themes and plugins from github.
+     * Author: PressLabs
+     * Author URI: http://www.presslabs.com/
+     * Version: 1.0.0
+     */
 
-require_once( 'classes/GitHub2WP.php' );
+    namespace github2wp;
 
-$github2Wp = new GitHub2WP();
+    use classes\Loader;
+
+    spl_autoload_register( function ( $className ) {
+        $className = __DIR__ . '/' . str_replace( '\\', DIRECTORY_SEPARATOR, $className );
+        $className .= '.php';
+
+        if ( file_exists( $className ) )
+            include $className;
+    } );
+
+
+    $github2Wp = new Loader( __FILE__, array () );
+    $github2Wp->load();
