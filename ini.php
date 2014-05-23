31ac70363dd58522c4110cc7ee573c90b5e80cf0
krodyrobi <robi@presslabs.com>
2014-05-23 10:38:54 +0300

    Fixed indentation

--- ini.php
+++ ini.php
@@ -1,25 +1,25 @@
 <?php
-    /*
-     * Plugin Name: github2wp
-     * Plugin URI: http://wordpress.org/extend/plugins/github2wp/
-     * Description: Managing themes and plugins from github.
-     * Author: PressLabs
-     * Author URI: http://www.presslabs.com/
-     * Version: 1.0.0
-     */
+/*
+ * Plugin Name: github2wp
+ * Plugin URI: http://wordpress.org/extend/plugins/github2wp/
+ * Description: Managing themes and plugins from github.
+ * Author: PressLabs
+ * Author URI: http://www.presslabs.com/
+ * Version: 1.0.0
+ */
 
-    namespace github2wp;
+namespace github2wp;
 
-    use classes\Loader;
+use classes\Loader;
 
-    spl_autoload_register( function ( $className ) {
-        $className = __DIR__ . '/' . str_replace( '\\', DIRECTORY_SEPARATOR, $className );
-        $className .= '.php';
+spl_autoload_register( function ( $className ) {
+    $className = __DIR__ . '/' . str_replace( '\\', DIRECTORY_SEPARATOR, $className );
+    $className .= '.php';
 
-        if ( file_exists( $className ) )
-            include $className;
-    } );
+    if ( file_exists( $className ) )
+        include $className;
+} );
 
 
-    $github2Wp = new Loader( __FILE__, array () );
-    $github2Wp->load();
+$github2Wp = new Loader( __FILE__, array () );
+$github2Wp->load();
