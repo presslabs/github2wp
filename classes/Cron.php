58f4a8cc043de2e41bf31c3c3d247583186f957e
Krody Robi <robi@presslabs.com>
2014-07-03 09:44:14 +0300

    Added extra details to cron

--- classes/Cron.php
+++ classes/Cron.php
@@ -3,11 +3,37 @@
 namespace github2wp\classes;
 
 class Cron {
+	private Loader $initiator;
+	private GitUser $user;
 
-	public function __construct() {
+	//TODO Make the loader have access to the Cron OBJECT
+	public function __construct( Loader $initiator, GitUser $user ) {
 		//TODO set-up wp hooks for cron jobs
+		$this->initiator = $initiator;
+		$this->user = $user;
+
+		add_filter( 'cron_schedules', array($this, 'cron_add_6h') );
+		add_action( 'cron_hook', array($this, 'head_commit_cron') );
+		add_action( 'cron_hook', array($this, 'token_cron') );
 	}
 
 	//TODO figure out what was needed for the cron to function
 
+	public function cron_add_6h( $schedules ) {
+		$schedules['6h'] = array(
+			'interval' => 21600,
+			'display'  => 'Once every 6 hours' )
+		);
+
+		return $schedules;
+	}
+
+
+	public function token_cron() {
+		
+	}
+
+	public function head_commits_cron() {
+	
+	}
 }
