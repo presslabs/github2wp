043129c8a0aeb12ffac906591af1de5504d2e6a2
Krody Robi <robi@presslabs.com>
2014-07-03 09:57:28 +0300

    Remove error_logs

--- ini.php
+++ ini.php
@@ -14,17 +14,12 @@ use github2wp\classes\Loader;
 
 spl_autoload_register( function ( $className ) {
 
-	// error_log($className . " " . __NAMESPACE__);
-
 	if ( substr( $className, 0, strlen(__NAMESPACE__) ) === __NAMESPACE__ )
     	$className = substr($className, strlen(__NAMESPACE__));
 
     $className = trim($className);
     $className = __DIR__ . str_replace( '\\', DIRECTORY_SEPARATOR, $className );
     $className .= '.php';
-
-    // error_log('here');
-    // error_log($className);
 
     if ( file_exists( $className ) )
         include $className;
