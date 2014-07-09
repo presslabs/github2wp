3f7fc0adaa9e60f38d0a93fa798db6a8fa7321de
Krody Robi <robi@presslabs.com>
2014-07-09 13:22:37 +0300

    Made a simple check for repo visibility

--- classes/git/GitHub_API.php
+++ classes/git/GitHub_API.php
@@ -8,9 +8,58 @@ use User;
 
 class GitHub_API extends API {
 	//TODO fill the $api_base
-	const API_BASE = '';
+	const API_BASE = 'https://api.github.com/';
+
+	private $endpoints = array(
+		'availability_ping' => 'repos/:user/:repo/branches?access_token=:access_token'
+	); 
 
 	public function __construct( User $user ) {
-		parent::__construct( $user, self::API_BASE );
+		parent::__construct( $user );
 	}
+
+	public function checkRepoVisibility( Repo $repo ) {
+		//TODO edit this after a more clearer repo structure
+		$segments = array(
+			'repo' => 'dotfiles',
+			'user' => 'krodyrobi',
+			'access_token' => 'aaaa'
+		);
+
+		$ping_url = $this->getApiUrl( $this->endpoints['availability_ping'], $segments );
+
+		$args = array(
+			'method'      => 'GET',
+			'timeout'     => 50,
+			'redirection' => 5,
+			'httpversion' => '1.0',
+			'blocking'    => true,
+			'headers'     => array(),
+			'body'        => null,
+			'cookies'     => array()
+		);
+
+		$response = wp_remote_get( $url, $args );
+
+		if ( is_wp_error( $response ) )
+			return false;
+
+		$result = wp_remote_retrieve_body( $response );
+		$result = json_decode($result, true);
+
+		if ( 'Not Found' == $result['message'] )
+			return false;
+
+		return true;
+	}
+
+
+	public function getApiUrl($endpoint, array $segments) {
+		foreach( $segments as $seg => $value ) {
+			$endpoint = str_replace( ':'.$seg, $value, $endpoint );
+		}
+
+		return self::API_BASE . $endpoint;
+	}
+
 }
