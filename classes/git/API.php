f2c40011d1664424a88ed0ee90a9db7c107e7c9d
Krody Robi <robi@presslabs.com>
2014-07-23 10:33:30 +0300

    Fixed constant polymorphism in getApiUrl plus added a request wrapper function

--- classes/git/API.php
+++ classes/git/API.php
@@ -23,7 +23,43 @@ abstract class API {
 
 	public abstract function checkSubmodule( Repo $repo );
 	public abstract function updateSubmodules( Repo $repo );
-	
-	public abstract function getApiUrl($endpoint, array $segments);
+
+
+	public function getApiUrl($endpoint, array $segments) {
+		foreach( $segments as $seg => $value ) {
+			$endpoint = str_replace( ':'.$seg, $value, $endpoint );
+		}
+
+		return static::API_BASE . $endpoint;
+	}
+
+
+	public function makeRequest( $url='' ) {
+		if ( FALSE === filter_var( $url, FILTER_VALIDATE_URL ) )
+			throw new \InvalidArgumentException( "$url is not a valid url!" );
+
+		$args = array(
+			'method'      => 'GET',
+			'timeout'     => 50,
+			'httpversion' => '1.0',
+			'blocking'    => true,
+			'headers'     => array(),
+			'body'        => null,
+			'cookies'     => array()
+		);
+
+		$response = wp_remote_request( $url, $args );
+		if ( is_wp_error( $response ) )
+			throw new \Exception( "Request at $url returned an error: " . $response->get_error_message() );
+
+		$code = wp_remote_retrieve_response_code( $response );
+
+		if ( '200' !== $code )
+			throw new \Exception( "Error code $code received at $url." );
+
+		$body = wp_remote_retrieve_body( $response );
+
+		return $body;
+	}
 
 }
