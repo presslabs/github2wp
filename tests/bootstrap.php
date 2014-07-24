560e755eee7c03f0aee5c6a6209036641bec452d
Krody Robi <robi@presslabs.com>
2014-07-24 12:11:05 +0300

    Made bootstrap.php work

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,11 +5,11 @@ if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 
 require_once $_tests_dir . '/includes/functions.php';
 
-function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../ini.php';
-}
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+$GLOBALS['wp_tests_options'] = array(
+	'active_plugins' => array('github2wp/ini.php')
+);
 
-require $_tests_dir . '/includes/bootstrap.php';
+require_once $_tests_dir . '/includes/bootstrap.php';
+
 
 require 'vendor/autoload.php';
