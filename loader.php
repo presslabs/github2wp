65af7a9fa96392d54ea18f3e03ed4dc26b54e85c
Krody Robi <robi@presslabs.com>
2014-08-25 11:04:57 +0300

    Fixed constants #2

--- loader.php
+++ loader.php
@@ -1,7 +1,7 @@
 <?php
 
 define( 'GITHUB2WP_MAIN_FILE_NAME', 'github2wp');
-define( 'GITHUB2WP_MAIN_PLUGIN_FILE', dirname(__DIR__) . '/' . GITHUB2WP_MAIN_FILE_NAME . '.php' );
+define( 'GITHUB2WP_MAIN_PLUGIN_FILE', __DIR__ . '/' . GITHUB2WP_MAIN_FILE_NAME . '.php' );
 define( 'GITHUB2WP_PLUGIN_BASENAME', plugin_basename( GITHUB2WP_MAIN_PLUGIN_FILE ) );
 define( 'GITHUB2WP_INC_PATH', __DIR__ . '/inc/' );
 
@@ -9,7 +9,6 @@ define( 'GITHUB2WP_MAX_COMMIT_HIST_COUNT', 100 );
 define( 'GITHUB2WP_ZIPBALL_DIR_PATH', WP_CONTENT_DIR . '/uploads/' . GITHUB2WP_MAIN_FILE_NAME . '/' );
 define( 'GITHUB2WP_ZIPBALL_URL', home_url() . '/wp-content/uploads/' . GITHUB2WP_MAIN_FILE_NAME . '/' );
 define( 'GITHUB2WP', GITHUB2WP_MAIN_FILE_NAME );
-
 
 
 require_once( GITHUB2WP_INC_PATH . 'helpers.php' );
